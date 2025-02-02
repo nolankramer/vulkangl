82526e6b33823d310e9ee755487cc6c970f1d3f7
Nolan Kramer <nolan@infinitygeneration.com>
2025-02-02 13:46:47 -0800

    Cleanup header

--- src/vulkangl.cpp
+++ src/vulkangl.cpp
@@ -2,6 +2,40 @@
 
 #include <GLFW/glfw3.h>
 
+// Helper macro for error checking
+#define VK_CHECK(result) \
+    if (result != VK_SUCCESS) { \
+        std::cerr << "Vulkan error at " << __FILE__ << ":" << __LINE__ << std::endl; \
+        exit(EXIT_FAILURE); \
+    }
+
+#ifndef PFNGLGENMEMORYOBJECTSEXTPROC
+typedef void (GLAPIENTRY *  PFNGLGENMEMORYOBJECTSEXTPROC) (GLsizei n, GLuint *memoryObjects);
+#endif
+
+#ifndef PFNGLIMPORTMEMORYFDEXTPROC
+typedef void (GLAPIENTRY *  PFNGLIMPORTMEMORYFDEXTPROC) (GLuint memory, GLuint64 size, GLenum handleType, GLint fd);
+#endif
+
+#ifndef PFNGLTEXSTORAGEMEM2DEXTPROC
+typedef void (GLAPIENTRY *  PFNGLTEXSTORAGEMEM2DEXTPROC) (GLenum target, GLsizei levels, GLenum internalFormat, GLsizei width, GLsizei height, GLuint memory, GLuint64 offset);
+#endif
+
+#ifndef PFNGLGENSEMAPHORESEXTPROC
+typedef void (GLAPIENTRY *  PFNGLGENSEMAPHORESEXTPROC) (GLsizei n, GLuint *semaphores);
+#endif
+
+#ifndef PFNGLIMPORTSEMAPHOREFDEXTPROC
+typedef void (GLAPIENTRY *  PFNGLIMPORTSEMAPHOREFDEXTPROC) (GLuint semaphore, GLenum handleType, GLint fd);
+#endif
+
+// Declare function pointers
+PFNGLGENMEMORYOBJECTSEXTPROC glGenMemoryObjectsEXT = nullptr;
+PFNGLIMPORTMEMORYFDEXTPROC glImportMemoryFdEXT = nullptr;
+PFNGLTEXSTORAGEMEM2DEXTPROC glTexStorageMem2DEXT = nullptr;
+PFNGLGENSEMAPHORESEXTPROC glGenSemaphoresEXT = nullptr;
+PFNGLIMPORTSEMAPHOREFDEXTPROC glImportSemaphoreFdEXT = nullptr;
+
 void loadOpenGLExtensions() {
     if (!glewIsSupported("GL_EXT_memory_object") || !glewIsSupported("GL_EXT_semaphore")) {
         throw std::runtime_error("Required OpenGL extensions GL_EXT_memory_object or GL_EXT_semaphore are not supported.");
